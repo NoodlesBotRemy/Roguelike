0b76c8af71cada88b264a8e88c82262b7cd1d7a6
Remy Bryan <mymywhatsallthisthen@gmail.com>
2023-01-24 22:34:36 +1100

    Added attack animations

--- Roguelike/Assets/Characters/Player/Scripts/PlayerController.cs
+++ Roguelike/Assets/Characters/Player/Scripts/PlayerController.cs
@@ -9,6 +9,9 @@ public class PlayerController : MonoBehaviour
     public float maxSpeed = 8f;
     public float idleFriction = 0.9f;
     public string currentState;
+    public bool canMove = true;
+    public bool isAttackPressed;
+    public bool isAttacking;
     public enum CurrentDirection
     {
         UP,
@@ -42,13 +45,28 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        isAttacking = false;
+        isAttackPressed = false;
+        currentDirection = CurrentDirection.DOWN;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(moveInput != Vector2.zero)
+
+    }
+
+    void FixedUpdate()
+    {
+        if(canMove == true && moveInput != Vector2.zero)
         {
+            /*
+            Accelerate player while run direction is pressed
+            Do not exceed max velocity
+            */
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity + (moveInput * moveSpeed * Time.deltaTime), maxSpeed);
+
+            // Change animation state to movement
             if(moveInput.y < 0)
             {
                 ChangeAnimationState(WALK_DOWN);
@@ -72,44 +90,82 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            if(currentDirection == CurrentDirection.UP)
-            {
-                ChangeAnimationState(IDLE_UP);
-            }
-            else if(currentDirection == CurrentDirection.DOWN)
-            {
-                ChangeAnimationState(IDLE_DOWN);
-            }
-            else if(currentDirection == CurrentDirection.RIGHT)
-            {
-                ChangeAnimationState(IDLE_RIGHT);
-            }
-            else if(currentDirection == CurrentDirection.LEFT)
-            {
-                ChangeAnimationState(IDLE_LEFT);
-            }
-        }
-    }
-
-    void FixedUpdate()
-    {
-        if(moveInput != Vector2.zero)
-        /*
-        Accelerate player while run direction is pressed
-        Do not exceed max velocity
-        */
-        {
-            rb.velocity = Vector2.ClampMagnitude(rb.velocity + (moveInput * moveSpeed * Time.deltaTime), maxSpeed);
-        }
-        else
-        {
+            // Linearly interpolate between current velocity and zero
             rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, idleFriction);
+
+            // Change animation state to idle
+            if(!isAttacking)
+            {
+                if(currentDirection == CurrentDirection.UP)
+                {
+                    ChangeAnimationState(IDLE_UP);
+                }
+                else if(currentDirection == CurrentDirection.DOWN)
+                {
+                    ChangeAnimationState(IDLE_DOWN);
+                }
+                else if(currentDirection == CurrentDirection.RIGHT)
+                {
+                    ChangeAnimationState(IDLE_RIGHT);
+                }
+                else if(currentDirection == CurrentDirection.LEFT)
+                {
+                    ChangeAnimationState(IDLE_LEFT);
+                }
+            }
+        }
+
+        if(isAttackPressed)
+        {
+            isAttackPressed = false;
+
+            if(!isAttacking)
+            {
+                isAttacking = true;
+                if(currentDirection == CurrentDirection.UP)
+                {
+                    ChangeAnimationState(ATTACK_UP);
+                }
+                else if(currentDirection == CurrentDirection.DOWN)
+                {
+                    ChangeAnimationState(ATTACK_DOWN);
+                }
+                else if(currentDirection == CurrentDirection.LEFT)
+                {
+                    ChangeAnimationState(ATTACK_LEFT);
+                }
+                else if(currentDirection == CurrentDirection.RIGHT)
+                {
+                    ChangeAnimationState(ATTACK_RIGHT);
+                }
+            }
         }
     }
 
     void OnMove(InputValue moveValue)
     {
         moveInput = moveValue.Get<Vector2>();
+    }
+
+    void OnPrimary()
+    {
+        isAttackPressed = true;
+    }
+
+    void LockMovement()
+    {
+        canMove = false;
+    }
+
+    void AttackComplete()
+    {
+        isAttacking = false;
+        UnlockMovement();
+    }
+
+    void UnlockMovement()
+    {
+        canMove = true;
     }
 
     void ChangeAnimationState(string newState)
